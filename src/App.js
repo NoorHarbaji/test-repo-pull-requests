c0e76bb3e196a198418fd677daff78c983b646a4
VernitaJ <55649851+VernitaJ@users.noreply.github.com>
2020-01-26 11:09:44 +0100

    Heading
    Extra heading

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <h1>Hello again!</h1>
+      <img src="" />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
